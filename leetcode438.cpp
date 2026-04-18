6be05e90dd9774609e61ea945c827ed328c8ffab
Ashit Mallick <airdevil2003@gmail.com>
2026-04-18 10:43:58 +0530

    improved solution

--- leetcode438.cpp
+++ leetcode438.cpp
@@ -2,59 +2,53 @@
 using namespace std;
 
 int main(){
-    //input 
-    string s;
-    cin >> s;
-
+    // input 
     string p;
     cin >> p;
+
+    string s;
+    cin >> s;
 
     int n1 = s.size();
     int n2 = p.size();
 
     //logic
-    vector<int> result; // result holding vector
-
-    //edge case 1 handeling
-    if(n2 > n1) {
-        return 0; // In main, we return 0
+    vector<int> result ; // result holding vector 
+    
+    // freq map for p (master list)
+    vector<int> target(26,0);
+    for(char c : p){
+        target[c - 'a']++; // ASCII value 
     }
 
-    // freq map for n "master list"
-    vector<int> target(26,0); // Fixed syntax
-    for (char c : p){
-        target[c - 'a']++; //ASCII value
-    }
+    //freq map for s (window list)
+    vector<int> window(26,0);
+    int right = 0; //right pointer 
+    int left = 0; // left pointer 
 
-    // feq map for s "sliding window list"
-    vector<int> window(26,0); // Fixed syntax
-    int left = 0;
+    for(right = 0; right < n1; right++){
 
-    // We need a loop for 'right' to move through s
-    for(int right = 0; right < n1; right++) {
-        
-        // Add current character to window
-        window[s[right] - 'a']++;
+        window[s[right] - 'a']++; // expanding the window 
 
-        //window size exceeds 
-        if (right - left + 1 > n2){
-            window[s[left] - 'a']--; // removing the oldest elements
-            left++;
+        // window exceeds the p size
+        if( right - left + 1 > n2){
+            window[s[left] - 'a']--; // removing the oldest elemests first
+            left ++; // left pointer will be incremented
         }
 
-        //window size meets p.size
+        //window size meets p size
         if (right - left + 1 == n2){
-            if(window == target){
-                result.push_back(left);
+            if(window == target) 
+            {
+                result.push_back(left); // push the answer in the result vector
             }
         }
     }
-
-    // Loop to print result
-    for(int x : result) {
+    // output
+    for(int x: result){
         cout << x << " ";
     }
     cout << endl;
-
     return 0;
+    
 }
